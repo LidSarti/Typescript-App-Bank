892121f68d368e7a2c0d22ba48dd6b0586779227
Lídia Sarti <90859386+LidSarti@users.noreply.github.com>
2024-10-21 18:17:57 +0000

    ✨ feat: criação de instâncias para todos os tipos de conta

--- app.ts
+++ app.ts
@@ -1,9 +1,28 @@
 import { CompanyAccount } from './class/CompanyAccount'
 import { PeopleAccount } from './class/PeopleAccount'
+import { BonusAccount } from './class/BonusAccount';
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+const companyAccount = new CompanyAccount('Empresa XYZ', 12345);
+console.log('--- Company Account ---');
+companyAccount.deposit(100); 
+companyAccount.withdraw(50); 
+companyAccount.getLoan(200); 
+companyAccount.getBalance();
+
+console.log('');
+
+const bonusAccount = new BonusAccount('Cliente ABC', 67890);
+console.log('--- Bonus Account ---');
+bonusAccount.deposit(100);
+bonusAccount.getBalance();
+bonusAccount.withdraw(30);
+bonusAccount.getBalance();
+
+console.log('');
+
+const peopleAccount = new PeopleAccount(987654321, 'João Silva', 54321);
+console.log('--- People Account ---');
+peopleAccount.deposit(200);
+peopleAccount.getBalance();
+peopleAccount.withdraw(100); 
+peopleAccount.getBalance();
